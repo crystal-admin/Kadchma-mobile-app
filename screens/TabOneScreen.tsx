4eea3b95d6778362cfc7a1518339cece8586b178
crystal-admin <info@crystalat.co>
2022-06-21 04:44:07 +0100

    commented on style sheet

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -50,8 +50,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
     // color: "#000000",
   },
   title: {
